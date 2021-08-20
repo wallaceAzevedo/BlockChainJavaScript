f00edce9b4103c3d827e47f9e5fb016f381771a3
Emma Paris <eparis@atlassian.com>
2021-08-20 07:53:20 -0300

    .

---
+++ rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle {
+    contructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+
+    area() {
+        return this.with * this.height;
+    }
+}
+module.exports = Rectangle
